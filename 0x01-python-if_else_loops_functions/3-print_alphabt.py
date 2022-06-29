7bdf7b01375690b946777532fdb584ee4df0c0be
unknown <elzabetmunir>
2022-06-29 15:09:10 +0300

    alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for abc in range(97, 123):
+    if abc != 101 and abc != 113:
+        print("{:c}".format(abc), end='')
