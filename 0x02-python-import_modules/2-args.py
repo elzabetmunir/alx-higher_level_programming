0799ce9c0868902db82641fa00417104c2b7fbec
unknown <elzabetmunir>
2022-07-04 14:00:57 +0300

    reformated the if statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     num = len(argv) - 1
     print("{:d} {:s}{:s}".format(num,
-                                 "argument" if num <= 2 and num == 1
+                                 "argument" if num == 1
                                  else "arguments",
                                  "." if num == 0 else ":"))
     for a, s in enumerate(argv):
