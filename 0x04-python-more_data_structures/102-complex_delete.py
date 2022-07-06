fc2457146c685a0506a980cd1eb6194e0e9b4b38
unknown <elzabetmunir>
2022-07-06 16:12:57 +0300

    bouns

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
-
-def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
-    total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        arabic = digits[roman]
-        total += arabic if total < arabic * 5 else -arabic
-    return total
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
